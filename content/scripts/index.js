deeffd4b51925050bd2d4442ddea56ea85acdb88
Tyler Long <tyler4long@gmail.com>
2017-06-05 18:12:51 +0800

    Auto generate mermaid diagrams

--- content/scripts/index.js
+++ content/scripts/index.js
@@ -1,1 +1,8 @@
-window.mermaid.init(undefined, document.querySelectorAll('.lang-mermaid'))
+let url = null
+
+setInterval(() => {
+  if (url !== window.location.href) {
+    url = window.location.href
+    window.mermaid.init(undefined, document.querySelectorAll('.lang-mermaid'))
+  }
+}, 1000)
